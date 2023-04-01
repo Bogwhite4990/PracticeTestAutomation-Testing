34b0a4915249ef830f8050fee2d970e64206391a
Adrian Bogdan <103454208+Bogwhite4990@users.noreply.github.com>
2023-04-01 23:28:24 +0300

    Add files via upload

---
+++ test.cy.js
@@ -0,0 +1,48 @@
+describe("Exemplu unde", () => {
+    // it("Verific daca un URL contine ceva", () => {
+    //     cy.visit('https://www.digi24.ro/stiri/externe/ue/pasa-mercedes-a-fost-retinut-de-autoritatile-ucrainene-episcopul-acuzat-de-legaturi-cu-moscova-2302037');
+        
+    //     cy.url().should('include', '/stiri/') // verifica un segment
+    // })
+
+    // Test cu delay
+    // it('asteapta 10 secunde', () => {
+    //     cy.visit('https://google.com');
+        
+    //     cy.get('#L2AGLb').click();
+
+    //     cy.wait(10000); // 10 secunde
+
+    //     cy.get('.gLFyf').type('Au trecut 10 secunde')
+    // })
+
+    // it('selecteaza folosind un atribut', () => {
+    //     cy.visit('https://google.com');
+
+    //     cy.get('#L2AGLb').click();
+
+    //     cy.get('[alt="Google').should('be.visible'); //Selector atribut alt + assertion cu visible
+    // })
+
+    // it('fac un screenshot la pagina', () => {
+    //     cy.visit('https://google.com');
+
+    //     cy.screenshot(); // Face screenshot intr-un folder default
+    // })
+
+    // it('verific o valoare din input', () => {
+    //     cy.visit('https://google.com');
+        
+    //     cy.get('#L2AGLb').click();
+    //     const googleSearch = cy.get('.gLFyf'); // constanta
+
+    //     googleSearch.type('123');
+    //     googleSearch.should('have.value', '123'); // Asertion contine textul
+    // })
+
+    // it('verific o valoare din input', () => {
+    //     cy.visit('https://libris.ro/');
+
+    //     cy.get('#autoCompleteButton').should('have.class', 'onSearchClick');
+    // })
+})
